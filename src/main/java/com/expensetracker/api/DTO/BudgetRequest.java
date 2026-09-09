4177fd8c832d1c25c8c9dc47d9e70b4780430e16
tej789 <gotitej2005@gmail.com>
2026-09-09 17:11:06 +0530

    added Email Verification

--- src/main/java/com/expensetracker/api/DTO/BudgetRequest.java
+++ src/main/java/com/expensetracker/api/DTO/BudgetRequest.java
@@ -2,7 +2,6 @@ package com.expensetracker.api.DTO;
 
 import com.expensetracker.api.model.CategoryType;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.time.Month;
